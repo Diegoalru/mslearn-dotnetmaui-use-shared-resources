5872d5553b08c7acd05aa72ecd71b2e7433b83d4
Diego Alonso Rubí Salas <diegoalru@gmail.com>
2024-06-27 11:12:45 -0600

    Applying Global Styles.

--- exercise4/TipCalculator/CustomTipPage.xaml.cs
+++ exercise4/TipCalculator/CustomTipPage.xaml.cs
@@ -1,49 +1,54 @@
 namespace TipCalculator;
 
-public partial class CustomTipPage : ContentPage
+public partial class CustomTipPage
 {
     public CustomTipPage()
     {
         InitializeComponent();
 
-        billInput.TextChanged += (s, e) => CalculateTip(false, false);
-        roundDown.Clicked += (s, e) => CalculateTip(false, true);
-        roundUp.Clicked += (s, e) => CalculateTip(true, false);
+        BillInput.TextChanged += (_, _) => CalculateTip(false, false);
+        RoundDown.Clicked += (_, _) => CalculateTip(false, true);
+        RoundUp.Clicked += (_, _) => CalculateTip(true, false);
 
-        tipPercentSlider.ValueChanged += (s, e) =>
+        TipPercentSlider.ValueChanged += (_, e) =>
         {
-            double pct = Math.Round(e.NewValue);
-            tipPercent.Text = $"{pct}%";
+            var pct = Math.Round(e.NewValue);
+            TipPercent.Text = $"{pct}%";
             CalculateTip(false, false);
         };
     }
 
-    void CalculateTip(bool roundUp, bool roundDown)
+    private void CalculateTip(bool roundUp, bool roundDown)
     {
-        double t;
-        if (Double.TryParse(billInput.Text, out t) && t > 0)
+        if (!double.TryParse(BillInput.Text, out var t) || !(t > 0)) return;
+
+        var pct = Math.Round(TipPercentSlider.Value);
+        var tip = Math.Round(t * (pct / 100.0), 2);
+
+        var final = t + tip;
+
+        if (roundUp)
         {
-            double pct = Math.Round(tipPercentSlider.Value);
-            double tip = Math.Round(t * (pct / 100.0), 2);
-
-            double final = t + tip;
-
-            if (roundUp)
-            {
-                final = Math.Ceiling(final);
-                tip = final - t;
-            }
-            else if (roundDown)
-            {
-                final = Math.Floor(final);
-                tip = final - t;
-            }
-
-            tipOutput.Text = tip.ToString("C");
-            totalOutput.Text = final.ToString("C");
+            final = Math.Ceiling(final);
+            tip = final - t;
         }
+        else if (roundDown)
+        {
+            final = Math.Floor(final);
+            tip = final - t;
+        }
+
+        TipOutput.Text = tip.ToString("C");
+        TotalOutput.Text = final.ToString("C");
     }
 
-    void OnNormalTip(object sender, EventArgs e) { tipPercentSlider.Value = 15; }
-    void OnGenerousTip(object sender, EventArgs e) { tipPercentSlider.Value = 20; }
+    private void OnNormalTip(object sender, EventArgs e)
+    {
+        TipPercentSlider.Value = 15;
+    }
+
+    private void OnGenerousTip(object sender, EventArgs e)
+    {
+        TipPercentSlider.Value = 20;
+    }
 }
